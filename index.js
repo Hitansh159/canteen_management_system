2085e88de9d27ddef54998a09438f62a7d701e48
HITANSH DOSHI <>
2020-11-21 16:20:49 +0530

    Added node dependances
    added express, body-parser for react framework more depandancies might
    be added as requied.
    
    added index.js for server side scripting and handelling.

---
+++ index.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "sign_in.html"));
+});
+
+app.get("/signup", (req, res)=>{
+  res.sendFile(path.join(__dirname, "sign_up,htnl"));
+});
+
+app.listen(5000, () => {
+  console.log(`Server is running on port 5000.`);
+});
