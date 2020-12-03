cadc57e585e8425ac65275346278690f98251bba
HITANSH DOSHI <>
2020-12-03 19:58:01 +0530

    update food functin tested and working;added result field in sign_up
    update function in index.js is working
    also added result field in sign up page for feed back

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const path = require("path");
 const mysql = require('mysql');
 const express = require("express");
 const bodyParser = require("body-parser");
-const { send } = require("process");
 
 const app = express();
 
@@ -107,12 +106,13 @@ app.get("/items_edit", (req, res) => {
 });
 
 app.post("/items_edit", (req, res) => {
+  console.log("getting call");
   const { id, title, name, type, price } = req.body;
-  if (update_food(id, title, name, type, price)) {
-    console.log("hogaya!!")
-  }
-  else
-    console.log("bhai scene hai ")
+
+  res.send({
+    result : update_food(id, title, name, type, price) ? 1 : 0
+  });
+    
 });
 
 app.get("/edit", (req, res) => {
@@ -136,73 +136,114 @@ function connect() {
 
 function send_data(name, password, email, mobile, person, uid) {
 
-  var con_data = connect();
-
+  var conn = connect();
   var suc = true;
-  con_data.connect(function (err) {
-    if (err)
-      return err;
+  conn.connect(function (err) {
 
-    // console.log(name, password, email, mobile, person, uid);
-    var sql = `INSERT INTO \`data\`(\`username\`, \`password\`, \`uid\`, \`mobile\`, \`email\`, \`type\`) VALUES  ('${name}','${password}','${uid}',${mobile},'${email}','${person}')`;
+      if (err){
+        console.log(err);
+        suc = false;
+      }
 
-    con_data.query(sql, function (err, result) {
-      if (err)
-        return err;
+      // console.log(name, password, email, mobile, person, uid);
+      var sql = `INSERT INTO \`data\`(\`username\`, \`password\`, \`uid\`, \`mobile\`, \`email\`, \`type\`) VALUES  ('${name}','${password}','${uid}',${mobile},'${email}','${person}')`;
 
-      console.log("1 record inserted");
+      conn.query(sql, function (err, result) {
+        if (err){
+          console.log(err);
+          suc = false;
+        }
+
+        console.log("1 record inserted");
+      });
+
     });
-  });
+
   return suc;
 }
 
 function get_data(id, callback) {
 
-  var con_data = connect();
-
+  var conn = connect();
   var suc = true;
   var res = null;
-  con_data.connect(function (err) {
-    if (err)
-      return err;
 
+  conn.connect(function (err) {
+    if (err){
+      console.log(err)
+      suc = false;
+    }
     var sql = `SELECT * FROM \`data\` WHERE uid='${id}';`;
     // console.log(sql);
 
-    con_data.query(sql, function (err, result, field) {
-      if (err)
-        return err;
+    conn.query(sql, function (err, result, field) {
+      if (err){
+        console.log(err);
+        suc = false;
+      }
 
       res = result;
       callback(result);
     });
 
   });
-  return res;
-}
-
-function update_food(id, title, name, type, price) {
-  var con_data = connect();
-
-  var suc = true;
-  con_data.connect(function (err) {
-
-    if (err) {
-      console.log(err)
-      return suc;
-    }
-
-    var sql = `UPDATE food SET title = ${title}, name = ${name}, type = ${type}, price = ${price} WHERE fid = ${id};`;
-
-    con_data.query(sql, function (err, result) {
-      if (err) {
-        console.log(err);
-        return suc;
-      }
-      console.log("1 record updated");
-    });
-  });
   return suc;
 }
 
+function update_food(id, title, name, type, price) {
+  var conn = connect();
+  var suc = true;
 
+  conn.connect(function (err) {
+  
+
+    if (err) {
+      console.log(err)
+      suc = false;
+      return false;
+    }
+    
+
+    var sql = `UPDATE food SET name = '${name}', type = '${type}', price = ${price} WHERE fid = '${id}';`;
+
+    conn.query(sql, function (err, result) {
+  console.log("suc is "+ suc+" result is "+result);
+      
+      if (err) {
+        console.log(err);
+        suc = false;
+        return false;
+      }
+  
+      console.log("1 record updated");
+    });
+  });
+  
+  return suc;
+}
+
+function add_food(name, type, price){
+  
+  const conn = connect();
+  var suc = true;
+
+  conn.connect(function(err) {
+    if(err) {
+      console.log(err);
+      suc = false;
+    }
+
+    var sql = `INSERT INTO food(name, type, price ) VALUES (${name}, ${type}, ${price});`;
+
+    conn.query(sql, (err, result)=>{
+      if(err){
+        console.log(err);
+        suc = false;
+      }
+
+      console.log("updated!");
+    });
+  });
+  return suc;
+
+}
