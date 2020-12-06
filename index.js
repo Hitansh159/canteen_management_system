e684cfc523d2e1153eb15d96e81d81244e82110b
HITANSH DOSHI <>
2020-12-06 13:50:00 +0530

    updated index file for webpage paths

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const db_connect = require("./db_connect");
 
 const app = express();
 app.set('view engine', 'ejs');
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 function setup_get_requests(link, filename){
   app.get(link, (req, res)=>{
@@ -15,7 +15,7 @@ function setup_get_requests(link, filename){
 
 var login_list = { 
   "/" : "sign_in",
-  "/signup" : "sign_up.html"                
+  "/signup" : "sign_up"                
 };
 
 for(i in login_list ){
@@ -41,12 +41,12 @@ app.post("/", (req, res) => {
 // client views
 
 var client_pages = {
-  "/clientview" : "client_view.html",
-  "/profile" : "profile.html",
-  "/history" : "history.html",
-  "/order" : "order.html",
-  "/shedule" : "shedule.html" ,
-  "/confirm" : "confirm.html"
+  "/clientview" : "client_view",
+  "/profile" : "profile",
+  "/history" : "history",
+  "/order" : "order",
+  "/shedule" : "shedule" ,
+  "/confirm" : "confirm"
 };
 
 for( i in client_pages){
@@ -95,9 +95,9 @@ app.post("/clientview", (req, res) => {
 // admin views
 
 var admin_pages = {
-  "/adminview" : "adminView.html",
-  "/items_edit" : "item_edit.html",
-  "/edit" : "item.html"
+  "/adminview" : "adminView",
+  "/items_edit" : "item_edit",
+  "/edit" : "item"
 };
 
 for(url in admin_pages){
@@ -139,156 +139,3 @@ app.get("/view.js" ,(req,res)=>{
   res.sendFile(path.join(__dirname, "view.js"));
 });
 
-/*
-create a file name db_connect
-store all functions their and change calling in this file from 
-function_name to  db_connect.function_name 
-*/
-/*function connect() {
-  return mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "aadi"
-  });
-}
-
-function send_data(name, password, email, mobile, person, uid) {
-
-  var conn = connect();
-  var suc = true;
-  conn.connect(function (err) {
-
-      if (err){
-        console.log(err);
-        suc = false;
-      }
-
-      // console.log(name, password, email, mobile, person, uid);
-      var sql = `INSERT INTO \`data\`(\`username\`, \`password\`, \`uid\`, \`mobile\`, \`email\`, \`type\`) VALUES  ('${name}','${password}','${uid}',${mobile},'${email}','${person}')`;
-
-      conn.query(sql, function (err, result) {
-        if (err){
-          console.log(err);
-          suc = false;
-        }
-
-        console.log("1 record inserted");
-      });
-
-    });
-
-  return suc;
-}
-
-function get_data(id, callback) {
-
-  var conn = connect();
-  var suc = true;
-  var res = null;
-
-  conn.connect(function (err) {
-    if (err){
-      console.log(err)
-      suc = false;
-    }
-    var sql = `SELECT * FROM \`data\` WHERE uid='${id}';`;
-    // console.log(sql);
-
-    conn.query(sql, function (err, result, field) {
-      if (err){
-        console.log(err);
-        suc = false;
-      }
-
-      res = result;
-      callback(result);
-    });
-
-  });
-  return suc;
-}
-
-function update_food(id, title, name, type, price) {
-  var conn = connect();
-  var suc = true;
-
-  conn.connect(function (err) {
-  
-
-    if (err) {
-      console.log(err)
-      suc = false;
-      return false;
-    }
-    
-
-    var sql = `UPDATE food SET name = '${name}', type = '${type}', price = ${price} WHERE fid = '${id}';`;
-
-    conn.query(sql, function (err, result) {
-      
-      if (err) {
-        console.log(err);
-        suc = false;
-        return false;
-      }
-  
-      console.log("1 record updated");
-    });
-  });
-  
-  return suc;
-}
-
-function add_food(name, type, price){
-  
-  const conn = connect();
-  var suc = true;
-
-  conn.connect(function(err) {
-    if(err) {
-      console.log(err);
-      suc = false;
-    }
-
-    var sql = `INSERT INTO food(name, type, price ) VALUES (${name}, ${type}, ${price});`;
-
-    conn.query(sql, (err, result)=>{
-      if(err){
-        console.log(err);
-        suc = false;
-      }
-
-      console.log("updated!");
-    });
-  });
-  return suc;
-
-}
-
-function load_food(callback){
-  const conn = connect();
-  var suc = true;
-  var res = null;
-
-  conn.connect(function (err) {
-    if (err){
-      console.log(err)
-      suc = false;
-    }
-    var sql = `SELECT * FROM food ;`;
-
-    conn.query(sql, function (err, result, field) {
-      if (err){
-        console.log(err);
-        suc = false;
-      }
-
-      res = result;
-      callback(result);
-    });
-
-  });
-
-  return suc;
-}*/
